4d2e8e24963404f0e294c1771edb6db48a65fbf4
Victor Hugo <victor.hugolk10@gmail.com>
2022-03-30 20:19:51 -0300

    status do pc na tela

--- pcRamUsage.js
+++ pcRamUsage.js
@@ -1,21 +1,21 @@
 const os = require('os')
 
-setInterval(()=>{
+setInterval(() => {
     const {arch, platform, totalmem, freemem} = os;
-const tRAM = totalmem() / 1024 / 1024;
-const fRAM = freemem() / 1024 / 1024;
-const usage = (fRAM / tRAM) * 100
+    const tRAM = totalmem() / 1024 / 1024;
+    const fRAM = freemem() / 1024 / 1024;
+    const usage = (fRAM / tRAM) * 100
 
-const stats = {
-    OS: platform(),
-    Arch: arch(),
-    TotalRam: `${parseInt(tRAM)} MB`,
-    RamLivre: `${parseInt(fRAM)} MB`,
-    Usage: `${usage.toFixed(2)} %`
+    const stats = {
+        OS: platform(),
+        Arch: arch(),
+        TotalRam: `${parseInt(tRAM)} MB`,
+        RamLivre: `${parseInt(fRAM)} MB`,
+        Usage: `${usage.toFixed(2)} %`
 }
 
 console.clear()
 console.table(stats)
 exports.stats = stats;
-}, 1000)
+}, 5000)
 
